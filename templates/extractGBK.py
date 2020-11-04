d4e0c254d77ed3396289bd4e8791c21b6bb0df85
Sam Minot <sam@fredhutch.org>
2020-11-04 12:13:08 -0800

    Filter the operon table

--- templates/extractGBK.py
+++ templates/extractGBK.py
@@ -22,3 +22,13 @@ print("Read in %d records" % len(recs))
 print("Reading in operon data from ${summary_csv}")
 operon_df = pd.read_csv("${summary_csv}")
 print("Read in operon information for %d genes" % operon_df.shape[0])
+
+print("Filtering down to operons from this genome and contig")
+print(operon_df.head())
+operon_df = operon_df.query(
+    "genome_id == '${genome_id}'"
+).query(
+    "contig_name == '${contig_name}'"
+)
+
+print(operon_df)
