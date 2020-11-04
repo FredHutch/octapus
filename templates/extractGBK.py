f1c0eca9a5a8163b663eb9fc780194389146401e
Sam Minot <sam@fredhutch.org>
2020-11-04 11:48:46 -0800

    Remove f-strings

--- templates/extractGBK.py
+++ templates/extractGBK.py
@@ -3,20 +3,12 @@
 from Bio import SeqIO
 import pandas as pd
 
-# Capture input variables populated by the Nextflow controller
-genome_id = '${genome_id}'
-operon_context = '${operon_context}'
-contig_name = '${contig_name}'
-genome_name = '${genome_name}'
-annotation_gbk = '${annotation_gbk}'
-summary_csv = '${summary_csv}'
+print("Processing {genome_id} -- {genome_name}")
 
-print(f"Processing {genome_id} -- {genome_name}")
-
-print(f"Reading in annotations from {annotation_gbk}")
-recs = [rec for rec in SeqIO.parse(annotation_gbk, "genbank")]
+print("Reading in annotations from {annotation_gbk}")
+recs = [rec for rec in SeqIO.parse('{annotation_gbk}', "genbank")]
 print(f"Read in {len(recs):,} records")
 
-print(f"Reading in operon data from {summary_csv}")
-operon_df = pd.read_csv(annotation_gbk)
+print("Reading in operon data from {summary_csv}")
+operon_df = pd.read_csv("{summary_csv}")
 print(f"Read in operon information for {operon_df.shape[0]:,} genes")
