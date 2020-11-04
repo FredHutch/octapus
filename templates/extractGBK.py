0995a7297bf27ff0e22562cf7464e5c134d1df63
Sam Minot <sam@fredhutch.org>
2020-11-04 11:53:14 -0800

    Fix string interpolation

--- templates/extractGBK.py
+++ templates/extractGBK.py
@@ -3,12 +3,12 @@
 from Bio import SeqIO
 import pandas as pd
 
-print("Processing {genome_id} -- {genome_name}")
+print("Processing ${genome_id} -- ${genome_name}")
 
-print("Reading in annotations from {annotation_gbk}")
-recs = [rec for rec in SeqIO.parse('{annotation_gbk}', "genbank")]
+print("Reading in annotations from ${annotation_gbk}")
+recs = [rec for rec in SeqIO.parse('${annotation_gbk}', "genbank")]
 print("Read in %d records" % len(recs))
 
-print("Reading in operon data from {summary_csv}")
-operon_df = pd.read_csv("{summary_csv}")
+print("Reading in operon data from ${summary_csv}")
+operon_df = pd.read_csv("${summary_csv}")
 print("Read in operon information for %d genes" % operon_df.shape[0])
